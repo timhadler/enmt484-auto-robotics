8cf2f411b33c01c17ffd60a1a4b7737431cf4946
Joseph Green <josephgreen@Josephs-MacBook-Air-677.local>
2020-09-10 17:51:26 +1200

    Sorry getting used to using variance files

--- find_nonlinear_variance.m
+++ find_nonlinear_variance.m
@@ -1,31 +1,36 @@
-function [var] = find_variance(actual, model, win)
 % find_variance, finds the variance between two datasets
 % related in a non-linear way
-%   Detailed explanation goes here
+% Detailed explanation goes here
 
-n = length(actual);
-var = zeros(n, 1);
+% Sensor models
+clc, clear, close all;
 
+train1 = readmatrix("training1.csv");
+train2 = readmatrix("training2.csv");
+cal = readmatrix("calibration.csv");
 
-for i = 1:n
-    if (i - win/2 <= 1)
-        lower_a = actual(1:i);
-        lower_m = model(1:i);
-    else
-        lower_a = actual((i-win/2):i);
-        lower_m = model((i-win/2):i);
+% Split data, time t, true distance x, commanded velocity u
+% sensor data- 4 infrared sensors, 2 sonar sensors
+[idx_1, t_1, x_1, u_1, ir1_1, ir2_1, ir3_1, ir4_1, sn1_1, sn2_1] = split_data(train1);
+[idx_2, t_2, x_2, u_2, ir1_2, ir2_2, ir3_2, ir4_2, sn1_2, sn2_2] = split_data(train2);
+[idx_c, t_c, x_c, u_c, ir1_c, ir2_c, ir3_c, ir4_c, sn1_c, sn2_c] = split_data(cal);
+
+% Selectively ignore outliers
+isOut_ir1 = zeros(length(t_c), 1);
+isOut_ir2 = zeros(length(t_c), 1);
+for i=1:length(t_c)
+    if ir1_c(i) > 6
+        isOut_ir1(i) = 1;
     end
-        
-    if (i + win/2 > n)
-        upper_a = actual((i+1):end);
-        upper_m = model((i+1):end);
-    else
-        upper_a = actual((i+1):(i+win/2));
-        upper_m = model((i+1):(i+win/2));
+
+    if (ir1_c(i) < 0.3777 && x_c(i) > 2)
+        isOut_ir1(i) = 1;
     end
-       
-    diff = [lower_m; upper_m] - [lower_a; upper_a];
-    var(i) = mean(diff.^2);
+    
+    if (sn2_c(i) > 3.5)
+        isOut_ir2(i) = 1;
+    end
 end
-end
+
+
 
